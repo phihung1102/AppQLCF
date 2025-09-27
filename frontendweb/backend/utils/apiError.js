dca334a1e2aee77d0a28815c7b58df7db9029ba5
phihung1102 <phihung1102@gmail.com>
2025-09-27 22:47:43 +0700

    Update .gitignore and remove backend folder

--- frontendweb/backend/utils/apiError.js
+++
@@ -1,7 +0,0 @@
-export default class ApiError extends Error {
-    constructor(statusCode, message) {
-        super(message);
-        this.statusCode = statusCode;
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
